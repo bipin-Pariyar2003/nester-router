048579f3aff9f92d442e2d6372d52866970c9614
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-02-26 11:36:57 +0545

    modified theme and ui little

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,27 @@ import Overall from "./Views/Report/Overall";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NameProvider } from "./context/NameContext";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./Components/MuiTheme/theme";
 
 function App() {
   return (
-    <NameProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/report" element={<Report />}>
-            <Route path="name" element={<NameReport />} />
-            <Route path="name-date" element={<NameDate />} />
-            <Route path="overall" element={<Overall />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </NameProvider>
+    <ThemeProvider theme={theme}>
+      <NameProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/report" element={<Report />}>
+              <Route path="name" element={<NameReport />} />
+              <Route path="name-date" element={<NameDate />} />
+              <Route path="overall" element={<Overall />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </NameProvider>
+    </ThemeProvider>
   );
 }
 export default App;
---
+++ src/Components/MuiTheme/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+
+  typography: {
+    fontFamily: "cursive",
+  },
+});
+
+export default theme;
